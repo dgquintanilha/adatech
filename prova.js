7d4a0dce2a71a1f7f253fa8bd896298576f7b0db
Douglas Quintanilha <dq.quintanilha@gmail.com>
2023-09-27 21:17:25 -0300

    adaprova.js

--- prova.js
+++ prova.js
@@ -69,4 +69,70 @@ function converterHorario(horario) {
   const numeros = [12, 17, 15, 19, 22, 17, 19, 12];
   const solitarios = encontrarNumerosSolitarios(numeros);
   console.log("Números solitários:", solitarios); 
+
+/*
+4.Qual é a saída do código a seguir?
+let n = 4;
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+        console.log(j);
+    }
+}
+a. Não imprime nada
+
+b. 1 2 3
+
+c. 1 1 2 1 2 3 1 2 3
+
+d. 1 2 3 4
+
+e. 1 1 2 1 2 3 1 2 3 4
+
+Resposta Letra (e) :1121231234  
+*/
+
+
+/*
+5.Qual é o resultado da expressão booleana abaixo?
+!(true && false) || (false && true)
+a. true
+
+b. null
+
+c. false
+
+d. NaN
+
+e. undefined
+
+Resposta: resultado da expressão booleana é true.
+*/
+
+/*
+6.Qual o valor da saída do código abaixo?
+let x = 0;
+let a = 0;
+let b = -5;
+if (a > 0) {
+    if (b < 0) {
+        x = x + 5;
+    } else if (a > 5) {
+        x = x + 4;
+    } else {
+        x = x + 3;
+    }
+} else {
+    x = x + 2;
+}
+console.log(x);
+a) 0
+
+b) 2
+
+c) 3
+
+d) 4
+
+Resposta: (b) 2
+*/
   
